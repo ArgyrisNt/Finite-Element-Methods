40047c817ca4783c072429f63239648a2f29da51
Argyris Ntoumanis <60874797+ArgyrisNt@users.noreply.github.com>
2021-02-20 19:42:49 +0200

    Update FEM_parabolic (Crank-Nicolson).py

--- FEM_parabolic (Crank-Nicolson).py
+++ FEM_parabolic (Crank-Nicolson).py
@@ -1,6 +1,3 @@
-![GitHub Logo](/images/logo.png)
-Format: ![Alt Text](https://user-images.githubusercontent.com/60874797/108603737-5d341780-73b2-11eb-8be3-dccf9f1206eb.png)
-
 import numpy as np
 from skfem import *
 from skfem import accum
